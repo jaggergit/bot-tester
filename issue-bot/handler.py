7046e2dd70abefa927875a939deb2c8605c22718
Jose Gonzalez <jose@thor.attlocal.net>
2018-11-21 17:26:45 -0800

    Update to update issue on Github

--- issue-bot/handler.py
+++ issue-bot/handler.py
@@ -1,4 +1,5 @@
 import requests, json, os, sys
+from github import Github
 
 def handle(req):
     """handle a request to the function
@@ -32,4 +33,38 @@ def handle(req):
         sys.exit("Error from sentimentanalysis")
 
     sys.stderr.write("Json Response to Sever:\n" + str(res.json()) + "\n")
-    return res.json()
+
+    positive_threshold = os.getenv("positive_threshold", "0,2")
+
+    polarity = res.json()['polarity']
+
+    # Call back to Github to  apply the label.
+
+    apply_label(polarity, 
+        payload["issue"]["number"],
+        payload["repository"]["full_name"],
+        positive_threshold)
+
+    return "Repo: %s, issue: %s, polarity: %f" % (payload["repository"]["full_name"], payload["issue"]["number"], polarity)
+
+def apply_label(polarity, issue_number, repo, positive_threshold):
+
+    sys.stderr.write("->apply_label() polarity: %f issue# %s Repo: %s Threshold: %s \n" % (polarity, issue_number, repo, positive_threshold))
+    g = Github(os.getenv("auth_token"))
+    repo = g.get_repo(repo)
+    issue = repo.get_issue(issue_number)
+
+    has_label_positive = False
+    has_label_review = False
+    for label in issue.labels:
+        if label == "positive":
+            has_label_positive = True
+        if label == "review":
+            has_label_review = True
+
+    if polarity > float(positive_threshold) and not has_label_positive:
+        issue.set_labels("positive")
+        sys.stderr.write("Setting Positive Labe\n")
+    elif not has_label_review:
+        issue.set_labels("review")
+        sys.stderr.write("Setting Review Labe\n")
